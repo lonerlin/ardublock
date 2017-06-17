2a20672fb292cc7e41902ff0cc7f93fb78051f18
lin <linjungui@126.com>
2017-06-17 10:18:10 +0800

    测试debounce成功

--- src/main/java/com/ardublock/translator/block/nhedu/DebounceBlock.java
+++ src/main/java/com/ardublock/translator/block/nhedu/DebounceBlock.java
@@ -5,17 +5,19 @@ import com.ardublock.translator.block.exception.BlockException;
 import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 /**
- * Created by Administrator on 2017/6/16 0016.
+ * 去抖函数，通过去抖方式读取一个输入端口的数字变量
+ * @author Lin Jungui
+ * @Time 2017/6/16 0016.
  */
 public class DebounceBlock extends TranslatorBlock{
 
 
-    public static final String NHEDU_DEBOUNCEBLOCK_DEFINE="const int debounceDelay=30;\n"+
+    private static final String NHEDU_DEBOUNCEBLOCK_DEFINE="const int debounceDelay=30;\n"+
             "bool __debounce(int pin)\n" +
             " {\n" +
             "   bool state;\n" +
             "   bool previousState;\n" +
-            "   pinMode(pinNumber, INPUT);\n"+
+            "   pinMode(pin, INPUT);\n"+
             "   previousState=digitalRead(pin);\n" +
             "   for(int counter=0;counter<debounceDelay;counter++)\n" +
             "   {\n" +
