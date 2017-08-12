0efcb8f6630757dbf1406dc28db1e05a90b7ed0b
lin <vsnetlin@163.com>
2017-08-12 13:29:58 +0800

    add Context class to git

---
+++ src/main/java/com/ardublock/ui/listener/OpenblocksFrameListener.java
@@ -0,0 +1,10 @@
+package com.ardublock.ui.listener;
+
+public interface OpenblocksFrameListener {
+	public void didNew();
+	public void didSave();
+	public void didSaveAs();
+	public void didLoad();	
+	public void didGenerate(String source);
+	public void didSendCode(String source);
+}
