08d01ca1b08dabf157c768618d9719a11a661050
lin <vsnetlin@163.com>
2018-04-23 21:38:54 +0800

    Add chopper constant current stepper motor blocks code.

---
+++ src/main/java/com/ardublock/translator/block/nhedu/CCStepperDirection.java
@@ -0,0 +1,23 @@
+package com.ardublock.translator.block.nhedu;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class CCStepperDirection extends TranslatorBlock {
+    public CCStepperDirection(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+        super(blockId, translator, codePrefix, codeSuffix, label);
+    }
+
+    @Override
+    public String toCode() throws SocketNullException, SubroutineNotDeclaredException, BlockException {
+        String stepperDirection;
+        TranslatorBlock translatorBlock=this.getRequiredTranslatorBlockAtSocket(0);
+        stepperDirection=translatorBlock.toCode();
+        String ret="setSteppersDirection("+stepperDirection+")\n";
+        return codePrefix+ret+codeSuffix;
+
+    }
+}
---
+++ src/main/java/com/ardublock/translator/block/nhedu/CCStepperIniBlock.java
@@ -0,0 +1,61 @@
+package com.ardublock.translator.block.nhedu;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.BlockException;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class CCStepperIniBlock extends TranslatorBlock {
+    public CCStepperIniBlock(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
+        super(blockId, translator, codePrefix, codeSuffix, label);
+    }
+
+    @Override
+    public String toCode() throws SocketNullException, SubroutineNotDeclaredException, BlockException {
+        TranslatorBlock translatorBlock=this.getRequiredTranslatorBlockAtSocket(0);
+        translator.addDefinitionCommand("#define  CCStepper_EN1  "+translatorBlock.toCode());
+        translatorBlock=this.getRequiredTranslatorBlockAtSocket(1);
+        translator.addDefinitionCommand("#define  CCStepper_DIR1  "+translatorBlock.toCode());
+        translatorBlock=this.getRequiredTranslatorBlockAtSocket(2);
+        translator.addDefinitionCommand("#define  CCStepper_STP1  "+translatorBlock.toCode());
+        translatorBlock=this.getRequiredTranslatorBlockAtSocket(3);
+        translator.addDefinitionCommand("#define  CCStepper_EN2  "+translatorBlock.toCode());
+        translatorBlock=this.getRequiredTranslatorBlockAtSocket(4);
+        translator.addDefinitionCommand("#define  CCStepper_DIR2  "+translatorBlock.toCode());
+        translatorBlock=this.getRequiredTranslatorBlockAtSocket(5);
+        translator.addDefinitionCommand("#define  CCStepper_STP2  "+translatorBlock.toCode());
+
+        translator.addSetupCommand("pinMode(CCStepper_EN1,OUTPUT);\n" +
+                "  pinMode(CCStepper_DIR1,OUTPUT);\n" +
+                "  digitalWrite(CCStepper_EN1,HIGH);\n" +
+                "  pinMode(CCStepper_EN2,OUTPUT);\n" +
+                "  pinMode(CCStepper_DIR2,OUTPUT);\n" +
+                "  digitalWrite(CCStepper_EN1,HIGH);\n");
+        translator.addDefinitionCommand("void setSteppersDirection(int direction)\n" +
+                "{\n" +
+                "  if(direction>0)\n" +
+                "  {\n" +
+                "       digitalWrite(CCStepper_DIR1,HIGH);\n" +
+                "       digitalWrite(CCStepper_DIR1,LOW);\n" +
+                "        analogWrite(CCStepper_STP1,128);\n" +
+                "        analogWrite(CCStepper_STP2,128);\n" +
+                "  }\n" +
+                "  else if(direction<0)\n" +
+                "  {\n" +
+                "       digitalWrite(CCStepper_DIR1,HIGH);\n" +
+                "       digitalWrite(CCStepper_DIR1,LOW);\n" +
+                "       analogWrite(CCStepper_STP1,128);\n" +
+                "       analogWrite(CCStepper_STP2,128);\n" +
+                "  }\n" +
+                "  else\n" +
+                "  {\n" +
+                "      analogWrite(CCStepper_STP1,0);\n" +
+                "      analogWrite(CCStepper_STP2,0);\n" +
+                "  }\n" +
+                "}\n");
+
+        return null;
+
+    }
+}
